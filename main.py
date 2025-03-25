1283f882fc5bb3d03b3c176e92adfee39a0f21ac
av2049 <alexvurbanov@gmail.com>
2025-03-25 23:14:11 +0000

    delete files

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import subprocess
-
-app = Flask(__name__)
-
-@app.route("/")
-def run_sim():
-    subprocess.run(["python", "run_sim.py"])
-    return "Simulation executed!", 200
